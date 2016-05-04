1e01d3983ac92363d38cddb26a4e08fa7d54bd6d
Ari Häyrinen <ari.hayrinen@gmail.com>
2016-05-04 22:16:46 +0300

    ui mockup

--- public/js/project.js
+++ public/js/project.js
@@ -1,9 +1,16 @@
 
+
+
+// place holder for node data
+var node = {"nodeid":"collection_basic","type":"collection","subtype":"basic","title":"väikkärit","usage":"","description":"Creates a new collection (table)","scripts":{"hello":"out.say('news', 'You added a collection node'); context.node.title = context.node.params.title","bye":"out.say('finish', 'Deleted collection node. Collection ' + context.node.params.collection + ' is deleted too. Bye!'); ","init":"var base_url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos';var format = '&format=json&nojsoncallback=?';out.url = base_url + '&photoset_id=' + context.node.params.album + format + '&api_key=' + context.node.params.apikey;out.say('news', out.url); ","run":"out.value = ''","finish":"out.say('finish', 'Collection created'); "},"views":{"params":"<label>Collection title </label><input type=\"text\" name=\"title\" id=\"title\" required /><label>Data description </label><textarea name=\"description\" id=\"description\" ></textarea>","settings":"no settings"},"type_desc":"Collection node creates the data holder (collection) for your data. This is the first node to add.","_id":"5729d14e7d0a45a715a77c1c","input_node":"","project":"5729d1417d0a45a715a77c1b","params":{"title":"väikkärit","description":"","collection":"p50_jyuvitskirjat_c0_vikkrit"},"collection":"p50_jyuvitskirjat_c0_vikkrit","number":0,"x":"351","y":"0"}
+
+
 var nodeList = function () {
 	var self = this;
 
-	this.collection = ko.observableArray(); // Initial items
+	this.collection = ko.observableArray(); 
 	this.projectNodes = ko.observableArray(); 
+	this.documents = ko.observableArray(); 
 
 	this.loadProject = function (nodesModel) {
 		$.getJSON("/get/nodes/" + self.currentProject, function(project) { 
@@ -34,6 +41,43 @@ var nodeList = function () {
 		})
 	}
 
+	
+	this.loadData = function () {
+		$.getJSON("/get/collection/" + node.collection, function(data) { 
+			self.collection([]);
+			for(var i = 0; i< data.length; i++) {
+				//data[i].vcc = self.params.skip_value + i + 1;
+				self.documents.push(data[i]);
+			}
+			//getCount();
+		})
+	}
+
+	this.sort = function (data, event) {
+		if(this.reverse) {
+			this.params.sort = '&reverse=1&sort=' + event.target.id;
+			this.reverse = false;
+		} else {
+			this.params.sort = '&sort=' + event.target.id;
+			this.reverse = true;
+		}
+			
+		this.collection([]);
+		this.params.skip_value = 0;
+		this.loadData();
+	};
+
+	this.nextPage = function() {
+		this.params.skip_func(25);
+		this.loadData();
+	};
+
+	this.prevPage = function() {
+		this.params.skip_func(-25);
+		this.loadData();
+	};
+		
+
 }
 
 $( document ).ready(function() {
@@ -43,11 +87,8 @@ $( document ).ready(function() {
 	ko.applyBindings(nodes);
 	//nodes.loadNodes(nodes);
 	nodes.loadProject(nodes);
+	nodes.loadData(nodes);
 	
-	var docs = new dataList();
-	docs.collectionName = path[path.length -1];
-	ko.applyBindings(docs);
-	nodes.loadData(docs);
 
 
 });
