0703ad048d4f05a8577d6355021fbb1bd0959cc6
arihayri <arihayri@debian>
2016-12-12 15:31:17 +0200

    DSpace node improvements

--- nodes/process_field_detect_language/run.js
+++ nodes/process_field_detect_language/run.js
@@ -2,7 +2,7 @@
 // context.data holds language detection result
 // context.doc holds current document
 
-if(context.data != null && context.data.constructor.name == "Array") {
+if(context.data != null && Array.isArray(context.data)) {
 	
 	var detects = [];
 	for (var i = 0; i < context.data.length; i++) {
@@ -14,12 +14,14 @@ if(context.data != null && context.data.constructor.name == "Array") {
 		// count succesfull detects
 		if(context.data[i] != null && context.data[i].reliable) {
 			context.node.success_counter++;
-			//out.say('progress', "LANGUAGE: " + context.data[i].languages[0].code); 
 			detects.push(context.data[i].languages[0].code);
 			
 		} else {
 			//out.say('progress', "LANGUAGE NOT DETECTED!");
-			detects.push(undefined);
+			if(i == 0)
+				detects.push(undefined);
+			else
+				break;
 		}
 		
 		
