48216f77e6acfc159adb96119d72ad7d04d5b207
arihayri <arihayri@debian>
2017-09-20 08:19:24 +0300

    enabled IP_passes for Shibboleth

--- app/project.js
+++ app/project.js
@@ -269,6 +269,17 @@ exports.authNode = function(req, res, next) {
 // checks that we have valid shibboleth user
 exports.shibbolethAuth = function(req, res, next) {
 
+	// first check if route should be open
+	var pass = false;
+	global.config.IP_passes.some(function(IP_pass) {
+		if(req.path.includes(IP_pass.path) && req.method === IP_pass.method && (req.ip === IP_pass.ip || IP_pass.ip === "*")) {
+			pass = true;
+			console.log("INFO: allowed by IP_pass: " + IP_pass.label)
+		}
+	})
+	if(pass)
+		return next();
+
 	var user = getUser(req);
 	if(user) {
 		if(global.config.shibbolethUsers.includes(user)) 
