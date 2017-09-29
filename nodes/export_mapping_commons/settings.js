0f9e9c4dd1ad13d26a724518e2e9595f366e86a8
Ari Häyrinen <ari.hayrinen@gmail.com>
2017-09-29 23:27:36 +0300

    commons upload node ui remake

--- nodes/export_mapping_commons/settings.js
+++ nodes/export_mapping_commons/settings.js
@@ -45,14 +45,14 @@ var wikiFields =
 
 // create inputs dynamically based template chosen			
 var sel = node.params.template;
+var table = "<table>";
 for(var i = 0; i < wikiFields[sel].length; i++) { 
-   var label = '<label>' + wikiFields[sel][i] + '</label>';  
-   var input1 = '<input name=\"_wt_1_' + wikiFields[sel][i] + '\" class=\"node-settings middle_input\" /><span > + </span>'; 
-   var input2 = '<input name=\"_wt_2_' + wikiFields[sel][i] + '\" class=\"node-settings dynamic_field middle_input\" /> <span > + </span>'; 
-   var input3 = '<input name=\"_wt_3_' + wikiFields[sel][i] + '\" class=\"node-settings short_input\" /><span > + </span>'; 
-   var input4 = '<input name=\"_wt_4_' + wikiFields[sel][i] + '\" class=\"node-settings dynamic_field short_input\" /><span > + </span>'; 
-   var input5 = '<input name=\"_wt_5_' + wikiFields[sel][i] + '\" class=\"node-settings short_input\" /><span > + </span>'; 
-
-   
-   $('#node_set___node_id__').append( label + input1 + input2 + input3 + input4 + input5); 
+	table += "<tr>";
+	table += "<td>" + wikiFields[sel][i] + "<td>";  
+	table += "<td><input name='" + wikiFields[sel][i] + "_static' class='node-settings middle_input' /></td>"; 
+	table += "<td><select name='" + wikiFields[sel][i] + "_dynamic' class='node-settings dynamic_field' ><option value=''>no value</option></select></td>"; 
+	table += "</tr>";
 }
+
+table += "<table>";
+$('#node_set___node_id__').append(table); 
