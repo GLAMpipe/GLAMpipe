1e8ff5b86d76d9191247a714e298bfd29ef92903
arihayri <ari.hayrinen@jyu.fi>
2018-04-13 09:04:05 +0300

    corrected quick install script

--- install_glampipe.sh
+++ install_glampipe.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 DIR="/glampipe-data"
 DATA_DIR=$PWD$DIR
+MONGO_VERSION=3.6
+DOCKERHUB="glampipe/glampipe:latest"
 
 echo "**************** GLAMpipe installation *******************"
 echo "* This will install GLAMpipe in Docker environment."
@@ -8,7 +10,7 @@ echo "* Installation downloads several hundreds of megabytes..."
 echo "* directory: " $DATA_DIR
 echo "**********************************************************"
 
-echo "Creating custom network for connecting mongo and GLAMpipe..."
+echo "Creating custom network for connecting Mongo and GLAMpipe..."
 
 if ! docker network ls|grep -q 'glampipe_net'
 	then docker network create --driver bridge glampipe_net
@@ -39,12 +41,12 @@ if docker ps|grep -wq 'glampipe_mongo'
 			else 
 				echo "Mongo container not found..."
 				echo  "Downloading and starting Mongo..."
-				docker run -d --network=glampipe_net --name=glampipe_mongo mongo:3.5
+				docker run -d --network=glampipe_net --name=glampipe_mongo mongo:$MONGO_VERSION
 		fi		
 fi
 
 echo "Checking new version of GLAMpipe..."
-docker pull glampipe/glampipe:latest
+docker pull $DOCKERHUB
 
 
 if docker ps|grep -wq 'glampipe'
@@ -52,10 +54,10 @@ if docker ps|grep -wq 'glampipe'
 		echo "GLAMpipe is running... I'll re-create the container"
 		docker stop glampipe
 		docker rm glampipe
-		docker run -d --network=glampipe_net --name glampipe -v glampipe-data:/glampipe-data -p 3000:3000 -e DOCKER=1 artturimatias/glampipe:dev bash -c 'node glampipe'
+		docker run -d --network=glampipe_net --name glampipe -v glampipe-data:/glampipe-data -p 3000:3000 -e DOCKER=1 $DOCKERHUB bash -c 'node glampipe'
 		docker logs -f glampipe
 	else
 		docker rm glampipe
-		docker run -d --network=glampipe_net --name glampipe -v glampipe-data:/glampipe-data -p 3000:3000 -e DOCKER=1 artturimatias/glampipe:dev bash -c 'node glampipe'
+		docker run -d --network=glampipe_net --name glampipe -v glampipe-data:/glampipe-data -p 3000:3000 -e DOCKER=1 $DOCKERHUB bash -c 'node glampipe'
 		docker logs -f glampipe
 fi
